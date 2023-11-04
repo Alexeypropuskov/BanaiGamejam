ce7cb08a2e9775a8d3a4b528868f59a4e0a0985d
d.lukichev <d.lukichev@corp.nekki.ru>
2023-11-04 20:29:08 +0300

    fix scenes

--- Assets/Scripts/Interfaces/LevelMover.cs
+++ Assets/Scripts/Interfaces/LevelMover.cs
@@ -1,13 +1,47 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace Interfaces
 {
 	public class LevelMover : MonoBehaviour
 	{
+		private Coroutine _coroutine;
+		
+		public Transform[] Foundations;
+		public int Num;
+		
+		public Camera Camera;
 		public Button NextTowerButton;
 
 		[Space]
 		public float SpeedMove;
+
+		private void Awake()
+		{
+			var found = FindObjectsOfType<Foundation>();
+			Foundations = new Transform[found.Length];
+			for (int i = 0; i < found.Length; i++)
+				Foundations[i] = found[i].transform;
+
+			Camera = FindObjectOfType<Camera>();
+			
+			//NextTowerButton.onClick.AddListener(StartMove());
+		}
+
+		public void StartMove()
+		{
+			Num++;
+			if (Num >= Foundations.Length)
+			{
+				
+				return;
+			}
+		}
+		
+		public IEnumerator MoveTo()
+		{
+			yield break;
+		}
 	}
 }
