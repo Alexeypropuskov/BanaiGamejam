6bcf8203a3f27ce8b8259a8c4019de4f60bf8bbb
d.lukichev <d.lukichev@corp.nekki.ru>
2023-11-04 16:36:00 +0300

    update logic game installer and add focus

--- Assets/Scripts/GameInstaller.cs
+++ Assets/Scripts/GameInstaller.cs
@@ -1,5 +1,8 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using Zenject;
 
 public class GameInstaller : MonoInstaller
@@ -8,19 +11,72 @@ public class GameInstaller : MonoInstaller
     
     public Controls Controls;
     public GameObject PausePanel;
+
+    [Space]
+    public GameObject _winPanel;
+    public Button _nextGameButton;
+    
+    [Space]
+    public GameObject _losePanel;
+    public Button _restartGameButton;
+
+    [Space]
+    public TextMeshProUGUI FallBlocks;
+    
+    [Space]
+    public int MinBlockFallForWin = 3;
     
     public override void InstallBindings()
     {
+        Controls = new Controls();
+        Controls.Enable();
+        
         _registry = FindObjectOfType<BlockRegistry>();
         Controls.Mouse.Pause.performed += OnPause;
         PausePanel.SetActive(false);
         
-        Controls = new Controls();
-        Controls.Enable();
         Container.BindInstance(Controls).AsSingle();
         TimeManager.IsGame = true;
+        
+        _winPanel.SetActive(false);
+        _losePanel.SetActive(false);
+        _nextGameButton.onClick.AddListener(LoadNextLevel);
+        _restartGameButton.onClick.AddListener(Restart);
+
+        FallBlocks.text = $"{_registry.FallBlocks.Count.ToString()}/{MinBlockFallForWin}";
     }
 
+    public void GameOver(ForcePoint point)
+    {
+        if (_registry.FallBlocks.Count >= MinBlockFallForWin)
+        {
+            _winPanel.SetActive(true);
+            TimeManager.IsGame = false;
+            return;
+        }
+        if (point.CurrentPower <= 0)
+        {
+            _losePanel.SetActive(true);
+            TimeManager.IsGame = false;
+            return;
+        }
+    }
+
+    private void LoadNextLevel()
+    {
+        var count = SceneManager.sceneCount;
+        var index = SceneManager.GetActiveScene().buildIndex;
+        index = (index + 1) % count;
+        
+        PlayerPrefs.SetInt(MainMenu.c_progressKey, index);
+        SceneManager.LoadScene(index);
+    }
+
+    private void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    
     private void OnPause(InputAction.CallbackContext obj)
     {
         TimeManager.IsGame = false;
